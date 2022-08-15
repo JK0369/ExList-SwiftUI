649133f20a0309fa3825ad209d75c82b2c19eb23
jake <palatable7@naver.com>
2022-08-16 00:32:52 +0900

    wip

--- ExList/ContentView.swift
+++ ExList/ContentView.swift
@@ -7,15 +7,44 @@
 
 import SwiftUI
 
+struct Ocean: Identifiable {
+  let name: String
+  let id = UUID()
+}
+
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
+  @State private var multiSelection = Set<UUID>()
+  
+  private var oceans = [
+    Ocean(name: "Pacific"),
+    .init(name: "Atlantic"),
+    .init(name: "Indian"),
+    .init(name: "Southern"),
+    .init(name: "Arctic"),
+  ]
+  
+  var body: some View {
+    // MARK: Basic
+//    List {
+//      Text("A List Item")
+//      Text("A Second List Item")
+//      Text("A Third List Item")
+//    }
+    
+    // MARK: Selections
+    NavigationView {
+      List(oceans, selection: $multiSelection) {
+        Text($0.name)
+      }
+      .navigationTitle("Oceans")
+      .toolbar { EditButton() }
     }
+    Text("\(multiSelection.count) selections")
+  }
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+  static var previews: some View {
+    ContentView()
+  }
 }
