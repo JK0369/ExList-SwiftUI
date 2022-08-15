e85efc2bb9b89bf16f87439e10d9240f7fcd90bb
jake <palatable7@naver.com>
2022-08-16 00:16:31 +0900

    Initial Commit

---
+++ ExList/ExListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExListApp.swift
+//  ExList
+//
+//  Created by 김종권 on 2022/08/16.
+//
+
+import SwiftUI
+
+@main
+struct ExListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
